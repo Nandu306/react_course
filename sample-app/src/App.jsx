dd14f67498f9afd19459e3944aa194c3eda399df
Nandu Rajagopala <82147496+Nandu306@users.noreply.github.com>
2022-06-17 13:58:24 +0200

    started react app

---
+++ sample-app/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar'
+import Main from './Main'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sample-app/src/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return(
+    <nav className="navbar-items">
+      <img src="./reactfacts.png" alt="logo"/>
+      <p>React Course - Project 1</p>
+    </nav>
+  )
+}
+
+export default Navbar
